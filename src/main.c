282b76e28bb662a5dc539f65e2d9ad4fe8268a4e
DinoNuggies7 <wwalrathIV@gmail.com>
2024-01-01 17:09:10 -0600

    the kitties now bounce off of each other

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ SDL_DisplayMode screen;
 
 typedef struct {
 	int x, y, w, h, timer, delay, frames;
-	float vx, vy, speed;
+	double vx, vy, speed;
 	unsigned char** gif;
 	unsigned int* gif_len;
 	SDL_Window* window;
@@ -15,34 +15,65 @@ int memes = 0;
 int tracklist[MEMELIMIT];
 GifWindow meme[MEMELIMIT];
 
+bool collideMeme(int i, int j) {
+	if (meme[i].x < meme[j].x + meme[j].w
+	 && meme[i].x + meme[i].w > meme[j].x
+	 && meme[i].y < meme[j].y + meme[j].h
+	 && meme[i].y + meme[i].h > meme[j].y) {
+		return true;
+	}
+	return false;
+}
+
 void spawnMeme() {
-	meme[memes].x = 0;
-	meme[memes].y = 0;
 	meme[memes].speed = rand() % 5 + 5;
 	meme[memes].vx = meme[memes].speed;
 	meme[memes].vy = meme[memes].speed;
-	meme[memes].w = rand() % 256 + 256;
-	meme[memes].h = rand() % 256 + 256;
+	bool done = false;
+	while (!done) {
+		done = true;
+		meme[memes].w = rand() % 300 + 100;
+		meme[memes].h = rand() % 300 + 100;
+		meme[memes].x = rand() % (screen.w - meme[memes].w);
+		meme[memes].y = rand() % (screen.h - meme[memes].h);
+		for (int i = 0; i < memes - 1; i++) {
+			if (collideMeme(memes, i))
+				done = false;
+		}
+	}
 	meme[memes].timer = 0;
 	meme[memes].delay = 100;
 	meme[memes].frames = (int)gifs_frames[tracklist[memes]];
 	meme[memes].gif = gifs[tracklist[memes]];
 	meme[memes].gif_len = gifs_len[tracklist[memes]];
-	meme[memes].window = SDL_CreateWindow("kitty cat :>", meme[memes].x, meme[memes].y, meme[memes].w, meme[memes].h, SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP);
+	meme[memes].window = SDL_CreateWindow("kitty cat >>>:3", meme[memes].x, meme[memes].y, meme[memes].w, meme[memes].h, SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP);
 	meme[memes].renderer = SDL_CreateRenderer(meme[memes].window, -1, SDL_RENDERER_PRESENTVSYNC);
 	meme[memes].texture = IMG_LoadTexture_RW(meme[memes].renderer, SDL_RWFromConstMem(meme[memes].gif[0], meme[memes].gif_len[0]), 1);
 	memes++;
 }
 
 void renderMeme(int i) {
-	if (meme[i].x < 0)
-		meme[i].vx = meme[i].speed;
-	else if (meme[i].x > screen.w - meme[i].w)
-		meme[i].vx = -meme[i].speed;
-	if (meme[i].y < 0)
-		meme[i].vy = meme[i].speed;
-	else if (meme[i].y > screen.h - meme[i].h)
-		meme[i].vy = -meme[i].speed;
+	for (int j = 0; j < memes; j++) {
+		if (i != j) {
+			if (collideMeme(i, j)) {
+				double tmp = meme[i].vx;
+				meme[i].vx = meme[j].vx;
+				meme[j].vx = tmp;
+
+				tmp = meme[i].vy;
+				meme[i].vy = meme[j].vy;
+				meme[j].vy = tmp;
+			}
+		}
+		if (meme[i].x <= 0)
+			meme[i].vx = meme[i].speed;
+		else if (meme[i].x >= screen.w - meme[i].w)
+			meme[i].vx = -meme[i].speed;
+		if (meme[i].y <= 0)
+			meme[i].vy = meme[i].speed;
+		else if (meme[i].y >= screen.h - meme[i].h)
+			meme[i].vy = -meme[i].speed;
+	}
 	meme[i].x += meme[i].vx;
 	meme[i].y += meme[i].vy;
 	SDL_SetWindowPosition(meme[i].window, meme[i].x, meme[i].y);
@@ -65,6 +96,7 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < MEMELIMIT; i++) {
 		tracklist[i] = i;
 	}
+
 	if (MEMELIMIT > 1) {
 		int _tmp[MEMELIMIT];
 		for (int i = 0; i < MEMELIMIT; i++) {
@@ -81,17 +113,17 @@ int main(int argc, char** argv) {
 	}
 
 	// Main Loop
-	// SDL_Event event;
+	SDL_Event event;
 	bool quit = false;
 	while (!quit) {
 		SDL_GetCurrentDisplayMode(0, &screen);
-		// while (SDL_PollEvent(&event)) { // Comment for release, so you can't close it
-		// 	if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
-		// 		quit = true;
-		// }
+		while (SDL_PollEvent(&event)) { // Comment for release, so you can't close it
+			if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+				quit = true;
+		}
 
-		// Spawn a Window/Meme every 2 seconds
-		int ticks = SDL_GetTicks() / 5000;
+		// Spawn a Window/Meme every 10 seconds
+		unsigned long ticks = SDL_GetTicks() / 10000;
 		if (ticks >= memes && memes < MEMELIMIT) {
 			spawnMeme();
 		}
